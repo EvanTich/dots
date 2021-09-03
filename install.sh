f389105b2a1aa4da1b6421d94e3084173018ae67
Evan Tichenor <evan.tichenor@gmail.com>
2021-09-03 01:04:16 -0400

    nicer messages, no sudo for paru

--- install.sh
+++ install.sh
@@ -19,6 +19,10 @@ gitinstall() {
     cd ..
 }
 
+installmsg() {
+    echo "Installing \`$1\` ($4 of $5) from $3. $1 $2"
+}
+
 # install all packages, inspired heavily by larbs
 install() {
     grep -v "^\s*#" pkglist.csv > /tmp/pkglist.csv
@@ -26,14 +30,25 @@ install() {
     while IFS=, read -r tag program comment; do
         n=$((n+1))
         comment=`sed -e 's/^\s*"//' -e 's/"$//' <<< "$comment"`
-        echo "Installing \`$program\` ($n of $total) from $tag. $program $comment"
         case "$tag" in
-            "A") sudo paru -S --noconfirm "$program" >/dev/null 2>&1 ;;
-            "G") echo "not supported yet..." ;; # gitinstall "$program" ;;
-            "P") pip install "$program" >/dev/null 2>&1 ;;
-            "C") cargo install "$program" >/dev/null 2>&1 ;;
-            "N") npm install -g "$program" >/dev/null 2>&1 ;;
-            *) sudo pacman --noconfirm --needed -S "$program" >/dev/null 2>&1 ;;
+        "A") 
+            installmsg $program $comment "the AUR" $n $total
+            paru -S --noconfirm "$program" >/dev/null 2>&1 ;;
+        "G") 
+            # installmsg $program $comment "git" $n $total
+            echo "not supported yet..." ;; # gitinstall "$program" ;;
+        "P") 
+            installmsg $program $comment "pip" $n $total
+            pip install "$program" >/dev/null 2>&1 ;;
+        "C") 
+            installmsg $program $comment "Cargo" $n $total
+            cargo install "$program" >/dev/null 2>&1 ;;
+        "N") 
+            installmsg $program $comment "npm" $n $total
+            npm install -g "$program" >/dev/null 2>&1 ;;
+        *) 
+            installmsg $program $comment "the AUR" $n $total
+            sudo pacman --noconfirm --needed -S "$program" >/dev/null 2>&1 ;;
         esac
     done < /tmp/pkglist.csv ;
 }
